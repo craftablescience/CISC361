f9a0b9bc2825dbf9e1625d78614ecbe6d15f454d
craftablescience <me@craftablescience.info>
2023-03-25 02:11:52 -0400

    fix: remove quotes around test arguments

--- src/test.h
+++ src/test.h
@@ -5,68 +5,68 @@
 
 #define test(name) static bool name(bool zxcvb_helper_failed_expected)
 
-#define assert_true(expr)                                                               \
-    do {                                                                                \
-        if (!(expr)) {                                                                  \
-            printf("Line %d: asserted \"%s\" to be true, was false!", __LINE__, #expr); \
-            return false;                                                               \
-        }                                                                               \
+#define assert_true(expr)                                                             \
+    do {                                                                              \
+        if (!(expr)) {                                                                \
+            printf("Line %d: asserted %s to be true, was false!\n", __LINE__, #expr); \
+            return false;                                                             \
+        }                                                                             \
     } while (0)
 
-#define assert_false(expr)                                                              \
-    do {                                                                                \
-        if (expr) {                                                                     \
-            printf("Line %d: asserted \"%s\" to be false, was true!", __LINE__, #expr); \
-            return false;                                                               \
-        }                                                                               \
+#define assert_false(expr)                                                            \
+    do {                                                                              \
+        if (expr) {                                                                   \
+            printf("Line %d: asserted %s to be false, was true!\n", __LINE__, #expr); \
+            return false;                                                             \
+        }                                                                             \
     } while (0)
 
-#define assert_eq(lhs, rhs)                                                            \
-    do {                                                                               \
-        if ((lhs) != (rhs)) {                                                          \
-            printf("Line %d: asserted \"%s\" to equal \"%s\"!", __LINE__, #lhs, #rhs); \
-            return false;                                                              \
-        }                                                                              \
+#define assert_eq(lhs, rhs)                                                      \
+    do {                                                                         \
+        if ((lhs) != (rhs)) {                                                    \
+            printf("Line %d: asserted %s to equal %s!\n", __LINE__, #lhs, #rhs); \
+            return false;                                                        \
+        }                                                                        \
     } while (0)
 
-#define assert_neq(lhs, rhs)                                                               \
-    do {                                                                                   \
-        if ((lhs) == (rhs)) {                                                              \
-            printf("Line %d: asserted \"%s\" to NOT equal \"%s\"!", __LINE__, #lhs, #rhs); \
-            return false;                                                                  \
-        }                                                                                  \
+#define assert_neq(lhs, rhs)                                                         \
+    do {                                                                             \
+        if ((lhs) == (rhs)) {                                                        \
+            printf("Line %d: asserted %s to NOT equal %s!\n", __LINE__, #lhs, #rhs); \
+            return false;                                                            \
+        }                                                                            \
     } while (0)
 
-#define expect_true(expr)                                                               \
-    do {                                                                                \
-        if (!(expr)) {                                                                  \
-            printf("Line %d: expected \"%s\" to be true, was false!", __LINE__, #expr); \
-            zxcvb_helper_failed_expected = false;                                       \
-        }                                                                               \
+#define expect_true(expr)                                                             \
+    do {                                                                              \
+        if (!(expr)) {                                                                \
+            printf("Line %d: expected %s to be true, was false!\n", __LINE__, #expr); \
+            zxcvb_helper_failed_expected = false;                                     \
+        }                                                                             \
     } while (0)
 
-#define expect_false(expr)                                                              \
-    do {                                                                                \
-        if (expr) {                                                                     \
-            printf("Line %d: expected \"%s\" to be false, was true!", __LINE__, #expr); \
-            zxcvb_helper_failed_expected = false;                                       \
-        }                                                                               \
+#define expect_false(expr)                                                            \
+    do {                                                                              \
+        if (expr) {                                                                   \
+            printf("Line %d: expected %s to be false, was true!\n", __LINE__, #expr); \
+            zxcvb_helper_failed_expected = false;                                     \
+        }                                                                             \
     } while (0)
 
-#define expect_eq(lhs, rhs)                                                            \
-    do {                                                                               \
-        if ((lhs) != (rhs)) {                                                          \
-            printf("Line %d: expected \"%s\" to equal \"%s\"!", __LINE__, #lhs, #rhs); \
-            zxcvb_helper_failed_expected = false;                                      \
-        }                                                                              \
+#define expect_eq(lhs, rhs)                                                      \
+    do {                                                                         \
+        if ((lhs) != (rhs)) {                                                    \
+            printf("Line %d: expected %s to equal %s!\n", __LINE__, #lhs, #rhs); \
+            zxcvb_helper_failed_expected = false;                                \
+        }                                                                        \
     } while (0)
 
-#define expect_neq(lhs, rhs)                                                               \
-    do {                                                                                   \
-        if ((lhs) == (rhs)) {                                                              \
-            printf("Line %d: expected \"%s\" to NOT equal \"%s\"!", __LINE__, #lhs, #rhs); \
-            zxcvb_helper_failed_expected = false;                                          \
-        }                                                                                  \
+#define expect_neq(lhs, rhs)                                                         \
+    do {                                                                             \
+        if ((lhs) == (rhs)) {                                                        \
+            printf("Line %d: expected %s to NOT equal %s!\n", __LINE__, #lhs, #rhs); \
+            zxcvb_helper_failed_expected = false;                                    \
+        }                                                                            \
     } while (0)
 
 #define test_end() return zxcvb_helper_failed_expected
@@ -85,7 +85,7 @@
             test = va_arg(args, bool(*)(bool));             \
         }                                                   \
         va_end(args);                                       \
-        printf("Passed %d/%d tests!", passed, total);       \
+        printf("Passed %d/%d tests!\n", passed, total);     \
     }                                                       \
     int main() {                                            \
         test_run_internal(__VA_ARGS__, NULL);               \
